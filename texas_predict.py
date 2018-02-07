b4bf01d163b3d95c53f8b060fcffcfd193272125
hushuofeng <hushuofeng08@gmail.com>
2018-02-07 20:51:48 +0800

    更新
    当多人弃牌时，可以正确预测概率变化

--- texas_predict.py
+++ texas_predict.py
@@ -9,8 +9,9 @@ import copy
 def predict_all(player_list, cards, repeat_num = 2000):
     """
     当全部玩家的手牌为明牌时，预测每名玩家的获胜概率
-    """
+    """    
     player_num = len(player_list) #记录玩家数量
+    player_drop = [player_list[x].drop for x in range(player_num)]
     result_count = [0] * player_num # 记录每位玩家获胜次数
     
     card_pr = tf.card_predict() # 生成预测牌桌
@@ -23,18 +24,33 @@ def predict_all(player_list, cards, repeat_num = 2000):
         card_pr.shuffle()   # 洗牌
         cards_tmp = cards + card_pr.deal(len(cards)) # 记录5张底牌
         # 生成每位玩家手牌
-        for i in range(player_num):
-            player_list[i].cardtype(cards_tmp)
+        for n in range(player_num):
+            player_list[n].cardtype(cards_tmp)
         result = tf.player_rank(player_list[:]) #记录获胜玩家
+        for m in range(player_num): #将弃牌玩家的排序置为100
+            if player_drop[m] == 1:
+                result[m] = 100
         for j in range(player_num): #更新每位玩家获胜次数
-            if result[j] == 0:
+            if result[j] == min(result):
                 result_count[j] += 1
-    return [format(x/repeat_num,'.2%') for x in result_count]
+    
+    output = []
+    for i in range(player_num):
+        if player_drop[i] == 0:
+            output.append(format(result_count[i]/repeat_num,'.2%'))
+        else :
+            output.append('Quit')
+    return output
 
-def predict_self(player, cards, player_num, repeat_num = 2000):
+def predict_self(player, cards, player_num, drop_num = 0, repeat_num = 2000):
     """
     当只知道player玩家的手牌时，预测该玩家的获胜概率
     """
+    if player.drop == 1:
+        return 'Quit'    
+    if player_num - drop_num== 1:
+        return format(1, '.2%')
+        
     card_pr = tf.card_predict() # 生成预测牌桌
     card_pr.remove_cards(player.handcard + cards)  # 从牌桌移除玩家的牌和底牌
     card_pr.change_nums(player_num-1) # 设定玩家个数
@@ -46,7 +62,7 @@ def predict_self(player, cards, player_num, repeat_num = 2000):
         card_pr.generate_usr_cardlist(0)   # 生成虚拟玩家手牌
         cards_tmp = cards + card_pr.deal(len(cards)) # 记录5张底牌
         player.cardtype(cards_tmp)  #生成player牌型
-        for j in range(player_num-1):     # 根据牌桌上玩家个数分发玩家手牌
+        for j in range(player_num-1-drop_num):     # 根据牌桌上玩家个数分发玩家手牌
             player_tmp.handcards(card_pr.usr_cardlist[j]) # 分发虚拟玩家手牌
             player_tmp.cardtype(cards_tmp[:])   #生成虚拟玩家牌型
             if tf.card_compare(player.handcard_type,player_tmp.handcard_type) == -1:
@@ -66,9 +82,11 @@ def predict_result(player_list, cards):
     symbol_num = player_num * 10 + 18
     
     self_prob = []
+    drop_num = sum([player_list[i].drop for i in range(player_num)])
+    print(drop_num)
     for i in range(player_num):
         self_prob.append(predict_self(copy.deepcopy(player_list[i]),\
-                                    cards[:],player_num))
+                                    cards[:],player_num,drop_num))
     all_prob = predict_all(copy.deepcopy(player_list),cards[:])
     
     rounds = ['第〇轮','','','第一轮','第二轮','最终轮']
